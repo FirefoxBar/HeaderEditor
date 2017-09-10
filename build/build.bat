c6ab21c24bbcff1ddd32bfa5f84d764597da43c6
ShuangYa <896640547@qq.com>
2017-09-10 16:13:16 +0800

    Widescreen management interface

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,3 @@
 @echo off
-REM call "D:/environment/bin/php/7.1.0/php.exe" "%~dp0/locales.php"
-call "D:/environment/bin/php/7.1.0/php.exe" "%~dp0/build.php"
+call "D:/environment/bin/php/7.1.9/php.exe" "%~dp0/build.php"
 pause
