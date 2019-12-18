a45ebe1f34e95bd9087b556401d258bf300228b2
ShuangYa <i@sylingd.com>
2019-12-18 12:36:30 +0800

    script: update lint

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -3,10 +3,11 @@ import { Nav } from '@alifd/next';
 import { t } from 'src/core/utils';
 
 export default () => {
-	return (
-		<Nav className="basic-nav" direction="hoz" type="primary" defaultSelectedKeys={['rules']}>
-			<Nav.Item key="rules">{t('rule_list')}</Nav.Item>
-			<Nav.Item key="options">{t('options')}</Nav.Item>
-		</Nav>
-	);
-}
+  return (
+    <Nav className="basic-nav" direction="hoz" type="primary" defaultSelectedKeys={['rules']}>
+      <Nav.Item key="rules">{t('rule_list')}</Nav.Item>
+      <Nav.Item key="options">{t('options')}</Nav.Item>
+      <Nav.Item key="export_and_import">{t('export_and_import')}</Nav.Item>
+    </Nav>
+  );
+};
