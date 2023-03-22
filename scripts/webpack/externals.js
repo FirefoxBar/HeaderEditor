f5c3436dad765f99cadfe9cef577d59030e521f1
泷涯 <i@sylingd.com>
2023-03-22 10:46:20 +0000

    test: add snapshot build

--- scripts/webpack/externals.js
+++ scripts/webpack/externals.js
@@ -16,21 +16,29 @@ const root = path.join(__dirname, '../..');
 
 module.exports = function (config) {
   const { version } = require(path.join(root, 'package.json'));
+
+  // 添加 snapshot 版本号
+  let versionText = version;
+  if (process.env.IS_SNAPSHOT && process.env.GITHUB_RUN_ID) {
+    versionText += '.' + process.env.GITHUB_RUN_ID;
+  }
+
+  // dev 环境复制 development 的 react 资源
   if (config.get('mode') === 'development') {
-    // dev 环境复制 development 的 react 资源
     copy.forEach((x) => {
       if (x.from.includes('.production.min.js')) {
         x.from = x.from.replace('.production.min.js', '.development.js');
       }
     });
   }
-  // 复制externals和静态文件
+
+  // 复制 manifest
   copy.push({
     from: './src/manifest.json',
     to: 'manifest.json',
     transform: (content) => {
       const jsonContent = JSON.parse(content);
-      jsonContent.version = version;
+      jsonContent.version = versionText;
 
       if (config.mode === 'development') {
         jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
@@ -39,6 +47,8 @@ module.exports = function (config) {
       return JSON.stringify(jsonContent);
     },
   });
+
+  // 复制其他静态文件
   config.plugin('copy').use(CopyWebpackPlugin, [copy]);
 
   // Add manaco into a standalone chunk
