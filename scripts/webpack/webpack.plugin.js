80725729dc6cd4a6e72c9426101af65ac64b4813
泷涯 <i@sylingd.com>
2023-04-02 15:39:31 +0000

    feat: remove background page

--- scripts/webpack/webpack.plugin.js
+++ scripts/webpack/webpack.plugin.js
@@ -2,5 +2,6 @@ module.exports = ({ context, registerCliOption, onGetWebpackConfig }) => {
   onGetWebpackConfig(config => {
     require('./externals')(config, context);
     require('./dev')(config, context);
+    require('./remove-html')(config, context);
   });
 };
