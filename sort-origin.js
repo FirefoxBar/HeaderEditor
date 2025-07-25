7cd38698df4e1994fc814bff3715477ef154fa80
sylingd <896640547@qq.com>
2025-07-25 14:15:58 +0000

    [skip ci] sync locale

--- sort-origin.js
+++ sort-origin.js
@@ -21,7 +21,7 @@ function ksort(obj) {
 
 let lang = require(path.join(__dirname, 'original/messages.json'));
 lang = ksort(lang);
-fs.writeFileSync(path.join(__dirname, 'original/messages.json'), JSON.stringify(lang, null, "  "), {
+fs.writeFileSync(path.join(__dirname, 'original/messages.json'), JSON.stringify(lang, null, "\t"), {
   encoding: "utf8"
 });
 console.log("Sort success");
